93976bfba824aaf1c60273f5e71f0ede7af16331
Evan Cutler Anway <evan.anway@gmail.com>
2016-12-18 20:45:25 -0500

    Update Ticket2Ride_cities.R

--- Ticket2Ride_cities.R
+++ Ticket2Ride_cities.R
@@ -47,22 +47,3 @@ plot(net_T2R,
      asp = 2/3, margin = c(0,0,0,0))
 
 dev.off()
-
-## END
-
-#mat_adjacency[which(mat_adjacency==0)] <- Inf # set all cells in the adjacency matrix with zero to infinity
-
-
-
-sym = isSymmetric(mat_cities) # test for matrix symmetry
-
-nodes = 1:nrow(mat_cities) # create a vector of node numbers
-
-arc_cities = Cmat2ArcList(nodes,mat_cities,directed = FALSE) # create a list of arcs between cities (useful for sparse matrices)
-
-mst_cities = getMinimumSpanningTree(nodes,arc_cities,algorithm = "Prim",
-                                    show.data=FALSE,show.graph=FALSE) # find the minimum spanning tree
-
-mat_cities_mst = ArcList2Cmat(mst_cities$tree.nodes,mst_cities$tree.arcs,directed=FALSE) # convert the resulting arc list to a matrix
-
-write.table(mat_cities_mst,file="E:/Personal/Experiments/Ticket2Ride/data/Ticket2Ride_mst.csv") # export the resulting matrix
